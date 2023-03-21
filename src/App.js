cd938ac141804d6d8827f34c1cd0ac1ca5426813
ewaschukmps <ewaschukmps@gmail.com>
2023-03-20 21:48:43 -0500

    WIP - 35:47

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
+import styles from './styles/modules/app.module.scss';
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <PageTitle>TODO List</PageTitle>
+      <div className={styles.app__wrapper}>
+        <AppHeader />
+      </div>
     </div>
   );
 }
---
+++ src/components/AppHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button, { SelectButton } from './Button';
+import styles from '../styles/modules/app.module.scss';
+
+function AppHeader() {
+  return (
+    <div className={styles.appHeader}>
+      <Button type="button" variant="primary">
+        Click Me
+      </Button>
+      <SelectButton id="status">
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Complete</option>
+      </SelectButton>
+    </div>
+  );
+}
+
+export default AppHeader;
